91759a1afa7fc618d445327ef1942e8224c2284f
omerthecoder <okanra@uaa.k12.tr>
2021-11-10 11:35:57 +0300

    Add project files.

---
+++ MayinTarlasi/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace MayinTarlasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flpMayinAlani = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.btnYeniOyun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // flpMayinAlani
+            // 
+            this.flpMayinAlani.Location = new System.Drawing.Point(0, 0);
+            this.flpMayinAlani.Name = "flpMayinAlani";
+            this.flpMayinAlani.Size = new System.Drawing.Size(260, 260);
+            this.flpMayinAlani.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(266, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Skor:";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(307, 9);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(0, 13);
+            this.lblScore.TabIndex = 1;
+            // 
+            // btnYeniOyun
+            // 
+            this.btnYeniOyun.Location = new System.Drawing.Point(269, 25);
+            this.btnYeniOyun.Name = "btnYeniOyun";
+            this.btnYeniOyun.Size = new System.Drawing.Size(103, 23);
+            this.btnYeniOyun.TabIndex = 2;
+            this.btnYeniOyun.Text = "Yeni Oyun";
+            this.btnYeniOyun.UseVisualStyleBackColor = true;
+            this.btnYeniOyun.Click += new System.EventHandler(this.btnYeniOyun_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 260);
+            this.Controls.Add(this.btnYeniOyun);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flpMayinAlani);
+            this.Name = "Form1";
+            this.Text = "Mayın Tarlası";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flpMayinAlani;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Button btnYeniOyun;
+    }
+}
+
---
+++ MayinTarlasi/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MayinTarlasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Button> btnlar = new List<Button>();
+        Random rnd = new Random();
+        int skor=0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblScore.Text = skor.ToString();
+            Doldur();
+        }
+        void Doldur()
+        {
+            int mayinSayisi = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                Button btn = new Button();
+                btn.Height = 20;
+                btn.Width = 20;
+                btn.Name = i.ToString();
+                btn.Tag = 0;
+                btnlar.Add(btn);
+                btn.Click += Btn_Click;
+                flpMayinAlani.Controls.Add(btn);
+
+            }
+            while (mayinSayisi < 20)
+            {
+                int sayi = rnd.Next(0, 100);
+                foreach (Button b in btnlar)
+                {
+                    if (b.Name==sayi.ToString())
+                    {
+                        b.Tag = 1;
+                        mayinSayisi++;
+                    }
+                }
+                //if ((int)btnlar[sayi].Tag==0)
+                //{
+                //    btnlar[sayi].Tag = 1;
+                //    mayinSayisi++;
+                //}
+            }
+        }
+        void Temizle()
+        {
+            flpMayinAlani.Controls.Clear();
+            skor = 0;
+            lblScore.Text = skor.ToString();
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if ((int)btn.Tag == 1)
+            {
+                btn.BackColor = Color.Red;
+                MessageBox.Show("Oyun bitti skorunuz:" + skor);
+                skor = 0;
+                lblScore.Text = skor.ToString();
+                OyunEngelle();
+            }
+            else
+            {
+                btn.BackColor = Color.Green;
+                skor += 10;
+                lblScore.Text = skor.ToString();
+            }
+        }
+        void OyunEngelle()
+        {
+            foreach (Button item in flpMayinAlani.Controls)
+            {
+                item.Enabled = false;
+            }
+        }
+
+        private void btnYeniOyun_Click(object sender, EventArgs e)
+        {
+            Temizle();
+            Doldur();
+        }
+    }
+}
